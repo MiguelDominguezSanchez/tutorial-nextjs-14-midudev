dc8faf4226021ce565e8951cf5557faf42cc6a85
Miguel Dominguez <miguel.dominguez.developer@gmail.com>
2023-11-26 19:57:21 +0100

    Continuando con el Straming

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,10 +3,12 @@ import { lusitana } from "../ui/fonts"
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { Suspense } from "react";
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from "../ui/skeletons";
 
 export default async function Page() {
-  const revenue = await fetchRevenue()
-  const latestInvoices = await fetchLatestInvoices()
+
+  
 
   return (
     <main>
@@ -24,8 +26,12 @@ export default async function Page() {
         /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-         <RevenueChart revenue={revenue}  /> 
-        <LatestInvoices latestInvoices={latestInvoices} /> 
+        <Suspense fallback={ <RevenueChartSkeleton /> }>
+         <RevenueChart /> 
+        </Suspense>
+        <Suspense fallback={ <LatestInvoicesSkeleton /> }>
+        <LatestInvoices   /> 
+        </Suspense>
       </div>
     </main>
   )}
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -34,6 +34,7 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
