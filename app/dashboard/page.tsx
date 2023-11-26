1c2e1bcde140c4d933f5d1d9155ee8f4380d85bd
Miguel Dominguez <miguel.dominguez.developer@gmail.com>
2023-11-26 18:40:26 +0100

    Chapter 6 - Setting Up Your Database

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
+import { fetchRevenue } from "../lib/data"
+
+export default async function Page() {
+  const revenue = await fetchRevenue()
+  console.log(revenue)
   return <p>Esta página es el contenido del Dashboard</p>
 }
