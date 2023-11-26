3a1aea7b6b65eee42d002341e17bfbeac13d3024
Miguel Dominguez <miguel.dominguez.developer@gmail.com>
2023-11-26 12:05:26 +0100

    Chapter 2 - CSS Styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import '@/app/ui/global.css';
+ 
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <h1>Hello World!</h1>
+      
       <body>{children}</body>
     </html>
   );
